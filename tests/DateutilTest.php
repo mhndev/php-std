3ebaac2815bd2acf50843dd1ec60b78f925d41b4
Majid Abdolhosseini <majid8911303@gmail.com>
2017-04-29 13:02:56 +0430

    unit test for DateUtil

---
+++ tests/DateutilTest.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is part of the mhndev\php-std package.
+ *
+ * (c) Majid Abdolhosseini <majid@mhndev.com>
+ */
+namespace mhndev\phpStd\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class DateUtilTest
+ * @package mhndev\phpStd\Tests
+ */
+class DateUtilTest extends TestCase
+{
+    function testToDateTime()
+    {
+
+    }
+
+
+    function testToIsoDate()
+    {
+
+    }
+
+}
