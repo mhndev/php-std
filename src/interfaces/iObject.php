ce6d8dcbba734a7b03ccda38d8c47522899e68fc
Majid Abdolhosseini <majid8911303@gmail.com>
2017-10-27 11:12:35 +0330

    add iObject interface

---
+++ src/interfaces/iObject.php
@@ -0,0 +1,15 @@
+<?php
+namespace mhndev\phpStd\interfaces;
+
+/**
+ * Interface iObject
+ * @package mhndev\phpStd\interfaces
+ */
+interface iObject
+{
+
+    /**
+     * @return array
+     */
+    function toArray();
+}
